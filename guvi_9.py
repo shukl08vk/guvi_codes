263e88be1553ffb3520e98fc6e507102836acd69
shukl08vk <21shukla08@gmail.com>
2018-02-04 14:28:04 +0530

    Update guvi_9.py

--- guvi_9.py
+++ guvi_9.py
@@ -1,15 +1,22 @@
 N=int(raw_input())
 K=int(raw_input())
+
 a=[]
 sum=0
-for i in range(N):
-        p=int(raw_input())
-        a.append(p)
-for i in range(K):
-        sum+=a[i]
+if N>0:
+        for i in range(N):
+                p=int(raw_input())
+                a.append(p)
+        if K>0:
+                for i in range(K):
+                        sum+=a[i]
+                print sum
+        else:
+                print "0"
 
-print sum
-
+        
+else:
+        print "0"
 
 
 
