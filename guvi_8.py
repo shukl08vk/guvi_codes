b1a42e99ca538c01600104f6301433352109ad6c
shukl08vk <21shukla08@gmail.com>
2018-02-04 14:19:31 +0530

    Update guvi_8.py

--- guvi_8.py
+++ guvi_8.py
@@ -1,10 +1,12 @@
 a=int(raw_input())
 sum=0
+if a>0:
+        for i in range(1,a+1):
+                sum+=i
+        print sum
+else:
+        print "0"
 
-for i in range(1,a+1):
-        sum+=i
-
-print sum
 
 
 
